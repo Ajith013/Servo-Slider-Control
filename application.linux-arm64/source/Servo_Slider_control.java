952f7abd675fc0e9fd268dec33898d02704d0445
Ajithkumar M <amanaparampil@gmail.com>
2019-02-08 11:07:51 -0800

    Adding update- value storage

--- application.linux-arm64/source/Servo_Slider_control.java
+++ application.linux-arm64/source/Servo_Slider_control.java
@@ -23,6 +23,8 @@ public class Servo_Slider_control extends PApplet {
 
 
 ControlP5 controlP5;
+Table table;
+String text;
 
 Arduino arduino;
 int servoAngle = 90;
@@ -48,6 +50,22 @@ controlP5.addSlider("servoAngleb",0,180,servoAngleb,20,90,180,20);
 controlP5.addSlider("servoAnglec",0,180,servoAngle,20,130,180,20);
 controlP5.addSlider("servoAngled",0,180,servoAngle,20,170,180,20);
 controlP5.addSlider("servoAnglee",0,180,servoAngle,20,210,180,20);
+controlP5.addButton("Save Angles",1,240,260,180,20);
+controlP5.addTextfield("Name",20,260,180,20);
+
+
+table = new Table();
+
+table.addColumn("ID");
+table.addColumn("servoAngle");
+table.addColumn("servoAnglea");
+table.addColumn("servoAngleb");
+table.addColumn("servoAnglec");
+table.addColumn("servoAngled");
+table.addColumn("servoAnglee");
+
+
+saveTable(table, "Desktop/new.csv");
 }
 public void draw() {
  arduino.analogWrite(9, servoAngle);
@@ -59,7 +77,25 @@ public void draw() {
  
  //delay(15);
 }
-  public void settings() {  size(400,400); }
+
+public void controlEvent(ControlEvent theEvent) {
+ if(theEvent.isController()){
+  if(theEvent.getController().getName()=="Save Angles"){
+    println("Data Saved to new.csv");
+     TableRow newRow = table.addRow();
+     text = controlP5.get(Textfield.class,"Name").getText();
+     newRow.setString("ID", text);
+     newRow.setFloat("servoAngle", servoAngle);
+     newRow.setFloat("servoAnglea", servoAnglea);
+     newRow.setFloat("servoAngleb", servoAngleb);
+     newRow.setFloat("servoAnglec", servoAnglec);
+    newRow.setFloat("servoAngled", servoAngled);
+     newRow.setFloat("servoAnglee", servoAnglee);
+     saveTable(table, "Values/new.csv");
+  }
+ }
+}
+  public void settings() {  size(450,400); }
   static public void main(String[] passedArgs) {
     String[] appletArgs = new String[] { "Servo_Slider_control" };
     if (passedArgs != null) {
--- application.linux-armv6hf/source/Servo_Slider_control.pde
+++ application.linux-armv6hf/source/Servo_Slider_control.pde
@@ -3,6 +3,8 @@ import cc.arduino.*;
 import controlP5.*;
 
 ControlP5 controlP5;
+Table table;
+String text;
 
 Arduino arduino;
 int servoAngle = 90;
@@ -13,7 +15,7 @@ int servoAngled = 90;
 int servoAnglee = 90;
 void setup() {
 
- size(400,400);
+ size(450,400);
 
  //println (Arduino.list());
  arduino = new Arduino(this, Arduino.list()[0], 57600);
@@ -28,6 +30,22 @@ controlP5.addSlider("servoAngleb",0,180,servoAngleb,20,90,180,20);
 controlP5.addSlider("servoAnglec",0,180,servoAngle,20,130,180,20);
 controlP5.addSlider("servoAngled",0,180,servoAngle,20,170,180,20);
 controlP5.addSlider("servoAnglee",0,180,servoAngle,20,210,180,20);
+controlP5.addButton("Save Angles",1,240,260,180,20);
+controlP5.addTextfield("Name",20,260,180,20);
+
+
+table = new Table();
+
+table.addColumn("ID");
+table.addColumn("servoAngle");
+table.addColumn("servoAnglea");
+table.addColumn("servoAngleb");
+table.addColumn("servoAnglec");
+table.addColumn("servoAngled");
+table.addColumn("servoAnglee");
+
+
+saveTable(table, "Desktop/new.csv");
 }
 void draw() {
  arduino.analogWrite(9, servoAngle);
@@ -38,4 +56,22 @@ void draw() {
  arduino.analogWrite(4, servoAnglec);
  
  //delay(15);
+}
+
+void controlEvent(ControlEvent theEvent) {
+ if(theEvent.isController()){
+  if(theEvent.getController().getName()=="Save Angles"){
+    println("Data Saved to new.csv");
+     TableRow newRow = table.addRow();
+     text = controlP5.get(Textfield.class,"Name").getText();
+     newRow.setString("ID", text);
+     newRow.setFloat("servoAngle", servoAngle);
+     newRow.setFloat("servoAnglea", servoAnglea);
+     newRow.setFloat("servoAngleb", servoAngleb);
+     newRow.setFloat("servoAnglec", servoAnglec);
+    newRow.setFloat("servoAngled", servoAngled);
+     newRow.setFloat("servoAnglee", servoAnglee);
+     saveTable(table, "Values/new.csv");
+  }
+ }
 }
